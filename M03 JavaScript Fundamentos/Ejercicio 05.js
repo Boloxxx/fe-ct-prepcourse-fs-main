e1d6922be70c3f0e1ec22ceccde6e5f8bfb6f2ec
boloxxx <bryanrodriguezalmonte@gmail.com>
2023-07-19 13:32:40 -0400

    probando

---
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -0,0 +1,96 @@
+/*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
+/*5️⃣ EJERCICIO 05 5️⃣*/
+
+function esPositivo(num) {
+   // La función recibe un entero. Devuelve como resultado un string que indica si el número
+   // es positivo o negativo.
+   // Si el número es positivo ---> "Es positivo".
+   // Si el número es negativo ---> "Es negativo".
+   // Si el número es 0, devuelve false.
+   // Tu código:
+
+   if (num > 0) {
+      return num = "Es positivo"
+
+   } else {
+      return num = "Es negativo"
+   }
+}
+
+function agregarSimboloExclamacion(str) {
+   // Agrega un símbolo de exclamación al final del string "str" y retórnalo
+   // Ejemplo: "hello world" ---> "hello world!"
+   // Tu código:
+   return str + "!";
+}
+
+function combinarNombres(nombre, apellido) {
+   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
+   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
+   // Tu código:
+
+   return nombre + " " + apellido;
+}
+
+function obtenerSaludo(nombre) {
+   // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
+   // Ejemplo: "Martin" ---> "Hola Martin!"
+   // Tu código:
+   let obtenerSaludo = "Hola " + nombre;
+}
+
+function obtenerAreaRectangulo(alto, ancho) {
+   // Retornar el área de un rectángulo teniendo su altura y ancho.
+   // Tu código:
+
+   let area = alto * ancho;
+   console.log(`El area del rectangulo es ${area}`);
+}
+
+function retornarPerimetro(lado) {
+   // La función recibe como argumento la medida de un lado de un cuadrado.
+   // Debes retornar su perímetro.
+   // Tu código:
+   let perimetro = lado * 4;
+   console.log(`El perimetro de un cuadrado es ${perimetro}`);
+}
+
+function areaDelTriangulo(base, altura) {
+   // Calcula el área de un triángulo y retorna el resultado.
+   // Tu código:
+   let areatr = base * altura / 2;
+   console.log(`El area del triangulo es ${areatr}`);
+}
+
+function deEuroAdolar(euro) {
+   // Supongamos que 1 euro equivale a 1.20 dólares.
+   // Debes calcular el valor recibido como argumento pasándolo a dolares.
+   // Tu código:
+   let resultado = euro * 1.2;
+   console.log(resultado)
+}
+
+function esVocal(letra) {
+   // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
+   // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
+   // Si no es vocal, tambien debe retornar "Dato incorrecto".
+   // Tu código:
+   if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+      console.log('Es vocal')
+     } else if (letra.length !== 1) {
+     console.log('Dato incorrecto')
+     }
+}
+
+/*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
+module.exports = {
+   esPositivo,
+   agregarSimboloExclamacion,
+   combinarNombres,
+   obtenerSaludo,
+   obtenerAreaRectangulo,
+   retornarPerimetro,
+   areaDelTriangulo,
+   deEuroAdolar,
+   esVocal,
+};
